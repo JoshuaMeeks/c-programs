3e54df8593cd7f0c6214604d4f4fce100540aca4
Joshua <jlmeeks587@gmail.com>
2023-10-04 15:30:35 -0400

    K&R C programming 1.2

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+    
+    printf("\nEnter an integer: ");
+    
+    // reads and stores input
+    scanf("%d", &number);
+
+    // displays	output
+    printf("You entered: %d\n\n", number);
+
+    return 0;
+}
